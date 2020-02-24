9fd378a1b729993774191ba2fa3afe4a64572289
Aviv Elkayam <avivelka@gmail.com>
2020-02-24 18:28:11 +0200

    movies_loder_file added

--- const.py
+++ const.py
@@ -4,16 +4,3 @@ NUM_OF_ITER = 10
 TOTAL_MOVIES = 7000
 API_PAGE_FILE_NAME = "api_page.txt"
 
-
-PROB_OF_LDA = [
-    (0, '0.010*"woods" + 0.010*"superhero" + 0.010*"life" + 0.010*"murder" + 0.009*"monster" + 0.009*"crime" + 0.008*"thriller" + 0.008*"house" + 0.008*"family" + 0.008*"revenge"'), 
-    (1, '0.017*"book" + 0.016*"anime" + 0.013*"space" + 0.013*"relationship" + 0.012*"novel" + 0.011*"race" + 0.010*"body" + 0.010*"car" + 0.009*"japan" + 0.009*"period"'), 
-    (2, '0.030*"police" + 0.015*"prison" + 0.015*"killer" + 0.013*"murder" + 0.012*"drugs" + 0.011*"town" + 0.011*"death" + 0.011*"island" + 0.011*"revenge" + 0.010*"novel"'), 
-    (3, '0.020*"tv" + 0.013*"zombie" + 0.011*"france" + 0.010*"film" + 0.010*"paris" + 0.009*"series" + 0.008*"news" + 0.007*"city" + 0.007*"sequel" + 0.006*"arts"'), 
-    (4, '0.051*"war" + 0.034*"woman" + 0.029*"director" + 0.029*"world" + 0.026*"story" + 0.025*"century" + 0.018*"history" + 0.017*"biography" + 0.013*"novel" + 0.013*"book"'),
-    (5, '0.027*"christmas" + 0.021*"comedy" + 0.019*"murder" + 0.015*"holiday" + 0.014*"book" + 0.012*"horror" + 0.010*"novel" + 0.009*"family" + 0.009*"sequel" + 0.008*"violence"'), 
-    (6, '0.022*"book" + 0.021*"drug" + 0.020*"novel" + 0.012*"story" + 0.009*"autobiography" + 0.008*"biography" + 0.008*"memoir" + 0.007*"conversion" + 0.006*"disorder" + 0.006*"century"'), 
-    (7, '0.040*"relationship" + 0.029*"gay" + 0.019*"wife" + 0.017*"lgbt" + 0.016*"husband" + 0.014*"friendship" + 0.014*"city" + 0.014*"york" + 0.012*"music" + 0.011*"story"'), 
-    (8, '0.055*"relationship" + 0.030*"school" + 0.023*"woman" + 0.019*"child" + 0.019*"mother" + 0.017*"director" + 0.016*"family" + 0.015*"sister" + 0.015*"drug" + 0.014*"teenage"'), 
-    (9, '0.019*"death" + 0.016*"art" + 0.014*"time" + 0.013*"superhero" + 0.011*"dc" + 0.010*"aftercreditsstinger" + 0.010*"travel" + 0.009*"murder" + 0.008*"universe" + 0.008*"duringcreditsstinger"')
-]
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from db.repo import movies_repo
 from topic_modeling.builder import TopicsBuilder
 import const
 
+
 def convert_tuples_to_dir(movies_data):
     data = {}
     for (movie_name, keyword_name) in movies_data:
@@ -20,7 +21,6 @@ def filter_data(movies_data):
 movies_data = movies_repo.get_movies_and_their_keywords()
 movies_data = convert_tuples_to_dir(movies_data)
 movies_data = filter_data(movies_data)
-print(len(movies_data))
 
 topics_builder = TopicsBuilder(movies_data)
 topics_builder.run_diagnose(10, 50)
@@ -148,20 +148,4 @@ for x in hundred_popular_movies:
 # print("lenght of topic 7: " , len(topic_7))
 # print("lenght of topic 8: " , len(topic_8))
 # print("lenght of topic 9: " , len(topic_9))
-
-
-# if not path.isfile(const.API_PAGE_FILE_NAME):
-#     with open(const.API_PAGE_FILE_NAME, "w+") as page_file:
-#         page_file.write("0")
-
-# # make api call and store in db every 20 seconds until target
-# while movies_repo.count_movies() < const.TOTAL_MOVIES:
-#     db_builder.build_movies_keywords_database(tmdb, movies_repo, 1)
-#     time.sleep(20)
-
-# topics_builder.build_document_term_matrix(movies_repo)
-# topics_builder.run_diagnose(const.NUM_OF_TOPICS, const.NUM_OF_ITER)
-
-# prob = topics_builder.get_probablities()
-# topics = topics_builder.get_topics()
 
